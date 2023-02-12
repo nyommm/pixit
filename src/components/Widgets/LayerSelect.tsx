15af944dcc858db68aa9354748ab7d3c691f271a
Jashandeep Singh <jashan.singh@outlook.in>
2023-02-12 11:58:16 +0530

    improved the layer select widget

--- src/components/Widgets/LayerSelect.tsx
+++ src/components/Widgets/LayerSelect.tsx
@@ -3,11 +3,35 @@ import './widget.css';
 
 import Layer from '../../pixit/Layer';
 
+interface LayerSelectItem {
+  activeLayer: string;
+  layerIds: string[];
+  setActiveLayer: Function;
+};
+
+function LayerSelectItem({ activeLayer, layerIds, setActiveLayer }: LayerSelectItem) {
+  const changeActiveLayer = (evt: React.MouseEvent<HTMLDivElement, MouseEvent>, id: string) => {
+    evt.stopPropagation();
+    setActiveLayer(id);
+  };
+  return (
+    <>
+      {layerIds.map((id) => (
+        <div key={id} 
+          className={`widget__item__layer ${id == activeLayer ? 'widget__item__layer-active' : ''}`}
+          onClick={(evt) => changeActiveLayer(evt, id)}>{id}
+        </div>
+      ))}
+    </>
+  )
+}
+
 function layerSelect(
   layers: Layer[], setLayers: Function, 
   activeLayer: string, setActiveLayer: Function) {
-  const insertLayer = () => {
-    const idx = layers.findIndex((layer) => layer.id === activeLayer);
+  const idx = layers.findIndex((layer) => layer.id === activeLayer);
+  const insertLayer = (evt: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    evt.stopPropagation();
     const active = `${layers.length}`; 
     setLayers([
       ...layers.slice(0, idx + 1),
@@ -16,12 +40,36 @@ function layerSelect(
     ]);
     setActiveLayer(active);
   };
+  const moveLayer = (evt: React.MouseEvent<HTMLSpanElement, MouseEvent>, change: number) => {
+    evt.stopPropagation();
+    // move layer down
+    if (change == 1) {
+      if (idx == layers.length - 1) return;
+      setLayers([
+        ...layers.slice(0, idx),
+        layers[idx + 1], layers[idx],
+        ...layers.slice(idx + 2)
+      ]);
+      }
+    // move layer up
+    if (change == -1) {
+      if (idx == 0) return;
+      setLayers([
+        ...layers.slice(0, idx - 1),
+        layers[idx], layers[idx - 1],
+        ...layers.slice(idx + 1)
+      ]);
+    }
+  }
   return () => (
     <div className="widget__item">
-      <select value={activeLayer} onChange={(evt) => setActiveLayer(evt.target.value)}>
-        {layers.map(({ id }) => <option key={id}>{id}</option>)}
-      </select>
-      <button onClick={insertLayer}>Add Layer</button>
+      <span className="widget__item__btn" onClick={insertLayer}>+</span>
+      <span className="widget__item__btn" onClick={(evt) => moveLayer(evt, 1)}>&#5169;</span>
+      <span className="widget__item__btn" onClick={(evt) => moveLayer(evt, -1)}>&#5167;</span>
+      <LayerSelectItem 
+        activeLayer={activeLayer}
+        layerIds={layers.map((layer) => layer.id)} 
+        setActiveLayer={setActiveLayer} />
     </div>
   );
 }
