eb978b60f565db3b666b7289c0c8e2d790d5c823
Jashandeep Singh <jashan.singh@outlook.in>
2023-02-25 19:51:42 +0530

    improved tool options styles

--- src/components/Widgets/toolOptions.tsx
+++ src/components/Widgets/toolOptions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ToolOptions } from '../../pixit/types';
+import './tool-options.css';
 
 function toolOptions(options: ToolOptions | undefined, setOptions: Function) {
   if (options == undefined) return () => null;
@@ -7,36 +8,43 @@ function toolOptions(options: ToolOptions | undefined, setOptions: Function) {
     return (
       <div className="widget__item">
         {options.thickness 
-          ? (<>
-              <input type="range" 
-                min={options.thickness.min} 
-                max={options.thickness.max} 
-                step={1}
-                value={options.thickness.value} 
-                onChange={(evt) => setOptions({...options, thickness: { ...options.thickness, value: evt.target.value }})} />
-              <span>{options.thickness.value}px</span>
-            </>)
+          ? (<div className="widget-toolOptions__thickness">
+              <label>Stroke: 
+                <input type="range" 
+                  min={options.thickness.min} 
+                  max={options.thickness.max} 
+                  step={1} className=""
+                  value={options.thickness.value} 
+                  onChange={(evt) => setOptions({...options, thickness: { ...options.thickness, value: evt.target.value }})} />
+              {options.thickness.value}px</label>
+            </div>)
           : null}
         {options.toolShape 
-          ? (<>
-              <label>Box
+          ? (<div className="widget-toolOptions__shape">
+              <label>Box: 
                 <input name="tool-shape" 
                   type="radio" 
-                  value='square' 
+                  value="square" 
+                  checked={options.toolShape == 'square'}
                   onChange={() => setOptions({ ...options, toolShape: 'square' })} />
               </label>
-              <label>Circle
+              <label>Circle: 
                 <input name="tool-shape" 
                   type="radio" 
-                  value='circle' 
+                  value="circle" 
+                  checked={options.toolShape == 'circle'}
                   onChange={() => setOptions({ ...options, toolShape: 'circle' })} />
               </label>
-            </>)
+            </div>)
           : null}
         {options.fill 
-          ? <input type="checkbox" 
-            checked={options.fill} 
-            onChange={(evt) => setOptions({ ...options, fill: evt.target.checked })} />
+          ? (<div className="widget-toolOptions__fill">
+              <label>Fill: 
+              <input type="checkbox" 
+                checked={options.fill} 
+                onChange={(evt) => setOptions({ ...options, fill: evt.target.checked })} />
+              </label>
+            </div>)
           : null}
       </div>
     )
